08584f783ca00d60da09010412542728fecb14e1
Anaissimpz <anaissimpenzwe@gmail.com>
2018-12-14 13:48:53 +0200

    Add function

---
+++ script.js
@@ -0,0 +1,5 @@
+function getResult(){
+    var side1= parseFloat(document.getElementById('side1').value);
+    var side1= parseFloat(document.getElementById('side2').value);
+    var side1= parseFloat(document.getElementById('side3').value);
+}
